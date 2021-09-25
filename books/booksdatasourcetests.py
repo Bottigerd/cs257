4ac53dae9e1ca1c9d86d5a5765a8825a6d131f23
CS Lab Account <cslab@olin310-03.mathcs.carleton.edu>
2021-09-25 17:05:47 -0500

    added test_unique_book

--- books/booksdatasourcetests.py
+++ books/booksdatasourcetests.py
@@ -14,14 +14,22 @@ class BooksDataSourceTester(unittest.TestCase):
         pass
 
     def test_unique_author(self):
-        authors = self.data_source.authors('Pratchett')
+        authors = self.data_source.authors('Pratchett') # Search Term that goes into books.py.
+        self.assertTrue(len(authors) == 1) # Sees if authors is an array with length 1.
+        self.assertTrue(authors[0] == Author('Pratchett', 'Terry')) # Sees if the first slot in the author array
+                                                                    # contains Pratchett and Terry.
+
+    def test_unique_book(self):
+        books = self.data_source.authors('Never')
         self.assertTrue(len(authors) == 1)
-        self.assertTrue(authors[0] == Author('Pratchett', 'Terry'))
+        self.assertTrue(authors[0] == Book('Neverwhere'))
 
     def test_startyear_equals_endyear(self):
         books = self.data_source.books_between_years(1939,1939)
         self.assertTrue(len(books) == 1)
         self.assertTrue(books[0] == Book('And Then There Were None')
+
+    #test merge comment
 
 if __name__ == '__main__':
     unittest.main()
