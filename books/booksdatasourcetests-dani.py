4a4589faa0e1edec7b76c25c07273840163ba752
CS Lab Account <cslab@olin310-04.mathcs.carleton.edu>
2021-09-25 17:42:31 -0500

    Added separate testing .py for Dani and Kiri

---
+++ books/booksdatasourcetests-dani.py
@@ -0,0 +1,33 @@
+'''
+   booksdatasourcetest.py
+   Jeff Ondich, 24 September 2021
+'''
+
+import booksdatasource
+import unittest
+
+class BooksDataSourceTester(unittest.TestCase):
+    def setUp(self):
+        self.data_source = booksdatasource.BooksDataSource('books1.csv')
+
+    def tearDown(self):
+        pass
+
+    def test_unique_author(self):
+        authors = self.data_source.authors('Pratchett') # Search Term that goes into books.py.
+        self.assertTrue(len(authors) == 1) # Sees if authors is an array with length 1.
+        self.assertTrue(authors[0] == Author('Pratchett', 'Terry')) # Sees if the first slot in the author array # contains Pratchett and Terry.
+
+    def test_unique_book(self):
+        books = self.data_source.books('Never')
+        self.assertTrue(len(books) == 1)
+        self.assertTrue(books[0] == Book('Neverwhere'))
+
+    def test_startyear_equals_endyear(self):
+        books = self.data_source.books_between_years(1939,1939)
+        self.assertTrue(len(books) == 1)
+        self.assertTrue(books[0] == Book('And Then There Were None'))
+
+if __name__ == '__main__':
+    unittest.main()
+
