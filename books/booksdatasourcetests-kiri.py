29aa5e1cfe4f7ba60847d2709279e286ae3b2a20
CS Lab Account <cslab@olin308-04.mathcs.carleton.edu>
2021-09-26 15:43:34 -0500

    Added 7 tests to booksdatasourcetests.py

--- books/booksdatasourcetests-kiri.py
+++ books/booksdatasourcetests-kiri.py
@@ -13,20 +13,40 @@ class BooksDataSourceTester(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_unique_author(self):
-        authors = self.data_source.authors('Pratchett') # Search Term that goes into books.py.
-        self.assertTrue(len(authors) == 1) # Sees if authors is an array with length 1.
-        self.assertTrue(authors[0] == Author('Pratchett', 'Terry')) # Sees if the first slot in the author array # contains Pratchett and Terry.
+    def test_different_years(self):
+        books = self.data_source.books_between_years(2018,2019)
+        self.assertTrue(len(books) == 2)
+        self.assertTrue(books[0] == Book('There, There'))
+        self.assertTrue(books[1] == Book('Fine, Thanks'))
 
-    def test_unique_book(self):
-        books = self.data_source.books('Never')
-        self.assertTrue(len(books) == 1)
-        self.assertTrue(books[0] == Book('Neverwhere'))
+    def test_comma_in_book(self):
+        books = self.data_source.books(',')
+        self.assertTrue(len(books) == 4)
+        self.assertTrue(books == ['Fine, Thanks','Right Ho, Jeeves','The Life and Opinions of Tristram Shandy, Gentleman','There, There'])
+        #might need to change the format of the above line because I am unsure how python checks equivalency between lists
 
-    def test_startyear_equals_endyear(self):
-        books = self.data_source.books_between_years(1939,1939)
-        self.assertTrue(len(books) == 1)
-        self.assertTrue(books[0] == Book('And Then There Were None'))
+    def test_startyear_earlier_than_endyear(self):
+        books = self.data_source.books_between_years(2019,2018)
+        self.assertTrue(len(books) == 41)
+        #honestly not sure what the desired outcome of this test should be, I think it depends on how we want to implement books_between_years
+
+    def test_multiple_authors_one_book(self):
+        books = self.data_source.books('Good Omens')
+        self.assertTrue(len(books[0].authors) == 2)
+
+    def test_alive_author(self):
+        authors = self.data_source.authors('Morrison')
+        self.assertTrue(len(authors) == 1) #okay so I'm not sure whether the length of authors actually should be 2 because Toni Morrison wrote 2 books but she herself is only 1 author
+        self.assertTrue(authors[0] == Author('Morrison','Toni'))
+
+    def test_endyear_only(self):
+        books = self.data_source.books_between_years(None,1813)
+        self.assertTrue(len(books) == 3)
+        self.assertTrue(books[0] == Book('The Life and Opinions of Tristram Shandy, Gentleman'))
+
+    def test_empty_author(self):
+        authors = self.data_source.authors()
+        self.assertTrue(authors == []) #yes this test will already pass but I'm not sure what else we would want an empty search to output
 
 if __name__ == '__main__':
     unittest.main()
--- books/booksdatasourcetests.py
+++ books/booksdatasourcetests.py
@@ -60,6 +60,41 @@ class BooksDataSourceTester(unittest.TestCase):
         self.assertTrue(len(authors) == 3)
         self.assertTrue(authors[0] == Author('Brontë', 'Ann'))
 
+    def test_different_years(self):
+        books = self.data_source.books_between_years(2018,2019)
+        self.assertTrue(len(books) == 2)
+        self.assertTrue(books[0] == Book('There, There'))
+        self.assertTrue(books[1] == Book('Fine, Thanks'))
+
+    def test_comma_in_book(self):
+        books = self.data_source.books(',')
+        self.assertTrue(len(books) == 4)
+        self.assertTrue(books == ['Fine, Thanks','Right Ho, Jeeves','The Life and Opinions of Tristram Shandy, Gentleman','There, There'])
+        #might need to change the format of the above line because I am unsure how python checks equivalency between lists
+
+    def test_startyear_earlier_than_endyear(self):
+        books = self.data_source.books_between_years(2019,2018)
+        self.assertTrue(len(books) == 41)
+        #honestly not sure what the desired outcome of this test should be, I think it depends on how we want to implement books_between_years
+
+    def test_multiple_authors_one_book(self):
+        books = self.data_source.books('Good Omens')
+        self.assertTrue(len(books[0].authors) == 2)
+
+    def test_alive_author(self):
+        authors = self.data_source.authors('Morrison')
+        self.assertTrue(len(authors) == 1) #okay so I'm not sure whether the length of authors actually should be 2 because Toni Morrison wrote 2 books but she herself is only 1 author
+        self.assertTrue(authors[0] == Author('Morrison','Toni'))
+
+    def test_endyear_only(self):
+        books = self.data_source.books_between_years(None,1813)
+        self.assertTrue(len(books) == 3)
+        self.assertTrue(books[0] == Book('The Life and Opinions of Tristram Shandy, Gentleman'))
+
+    def test_empty_author(self):
+        authors = self.data_source.authors()
+        self.assertTrue(authors == []) #yes this test will already pass but I'm not sure what else we would want an empty search to output
+
 
 if __name__ == '__main__':
     unittest.main()
