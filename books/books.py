046b7eb24fd73125908d446908a095393fb1cb6e
CS Lab Account <cslab@olin310-24.mathcs.carleton.edu>
2021-09-30 19:38:42 -0500

    initial books.py draft

--- books/books.py
+++ books/books.py
@@ -1,2 +1,18 @@
 #Kiri and Dani books.py
 #Version 1 9/28/21
+
+import argparse
+from booksdatasource import Book, Author, BooksDataSource
+
+parser = argparse.ArgumentParser(description='Searches book database')
+
+#arguemnts
+parser.add_argument('searchTerm', metavar = 'searchTerm', type=str, help = 'search term for books database')
+
+#parsing the arugments
+args = parser.parse_args()
+
+#running the search
+booksdatasource = BooksDataSource('books1.csv')
+searchResults = booksdatasource.books(args.searchTerm)
+booksdatasource.printBooks(searchResults)
